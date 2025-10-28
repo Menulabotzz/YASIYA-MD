fa96305e417b03bf5b52582ce87d3b4a87f8a326
THARUWA <Sandarumax513@gmail.com>
2025-10-28 09:27:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~FdggQBoD#7QCT1jJVxNfnki1cNvhDqvuIC6XGqWBdf422uq4gVYU'
 };
